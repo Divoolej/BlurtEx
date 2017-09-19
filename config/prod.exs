4bf5eb6e17a9f116a1755ed8b808ed6e0bf95ead
Wojciech Olejnik <divoolej@gmail.com>
2017-09-20 00:44:59 +0200

    More stupid fixes

--- config/prod.exs
+++ config/prod.exs
@@ -17,7 +17,7 @@ config :blurtex, BlurtEx.Endpoint,
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
-  http: [:inet6, port: {:system, "PORT"}],
+  http: [:inet6, port: {:system, "PORT"}]
 
 # Do not print debug messages in production
 config :logger, level: :info
