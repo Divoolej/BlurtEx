26e4229c0c1a49a840005cbdae0d2a9493b26313
Wojciech Olejnik <divoolej@gmail.com>
2017-09-20 00:03:35 +0200

    Update the message sync on the backend side

--- web/channels/room_channel.ex
+++ web/channels/room_channel.ex
@@ -1,6 +1,9 @@
 defmodule BlurtEx.RoomChannel do
   use BlurtEx.Web, :channel
   alias BlurtEx.Presence
+  alias BlurtEx.User
+
+  intercept ["presence_diff"]
 
   def join("room:lobby", _, socket) do
     send self(), :after_join
@@ -8,7 +11,7 @@ defmodule BlurtEx.RoomChannel do
   end
 
   def handle_info(:after_join, socket) do
-    Presence.track(socket, socket.assigns.user, %{
+    Presence.track(socket, socket.assigns.user.username, %{
       online_at: :os.system_time(:milli_seconds)
     })
     push socket, "presence_state", Presence.list(socket)
@@ -16,11 +19,21 @@ defmodule BlurtEx.RoomChannel do
   end
 
   def handle_in("message:new", message, socket) do
+    new_message = if message == %{}, do: "", else: message
+    modified_at = DateTime.utc_now
     broadcast! socket, "message:new", %{
-      user: socket.assigns.user,
-      body: message,
-      timestamp: :os.system_time(:milli_seconds)
+      user_id: socket.assigns.user.id,
+      username: socket.assigns.user.username,
+      body: new_message,
+      timestamp: :os.system_time(:milli_seconds),
     }
+    changeset = Ecto.Changeset.change(socket.assigns.user, %{ message: new_message, modified_at: modified_at })
+    Repo.update!(changeset)
     { :noreply, socket }
+  end
+
+  def handle_out("presence_diff", msg, socket) do
+    push socket, "presence_diff", msg
+    {:noreply, socket}
   end
 end
